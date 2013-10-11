67cd667a41c974ddbba190d4d0bff878ccab2769
wtrdrnkr <mdmusslewhite@gmail.com>
2013-10-11 15:03:27 +0000

    changed packages list to '' (means root package?)

--- setup.py
+++ setup.py
@@ -5,11 +5,11 @@ except ImportError:
 # Not sure that all of the listed install_requires are actually required --Michael Musslewhite
 setup(
       name='PyRECONSTRUCT',
-      version='0.2.3',
+      version='0.2.4',
       author='Michael Musslewhite',
       author_email='mdmusslewhite@gmail.com',
       packages=[
-                'pyrecon'
+                ''
                 ],
       license='LICENSE.txt',
       description='Python tools for interacting with XML files associated with RECONSTRUCT',
