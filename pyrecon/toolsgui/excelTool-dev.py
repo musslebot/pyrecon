e5a1aec31bf8d77e31f9a466b0f7e261dd9b77be
wtrdrnkr <mdmusslewhite@gmail.com>
2013-10-29 14:36:48 +0000

    replaced QTableWidget with QListWidget, trying it out

--- pyrecon/toolsgui/excelTool-dev.py
+++ pyrecon/toolsgui/excelTool-dev.py
@@ -1,8 +1,9 @@
 from PySide import QtCore, QtGui
 from pyrecon.tools import classes
 import sys, os
-
+# THIS IS A DEVELOPMENT FILE AND SHOULD NOT BE EXPECTED TO WORK
 # Current issues:
+# - Initially loads list correctly, can't update with refresh
 # - Refresh button should be square with arrow-circle in it
 
 class ObjectListWidget(QtGui.QWidget):
@@ -10,58 +11,110 @@ class ObjectListWidget(QtGui.QWidget):
         QtGui.QWidget.__init__(self, parent)
         # Size (1/3 of total width?) #=== how to make scalable?
         self.setGeometry(0,0,width,height)
-        self.initUI()
         
-    def initUI(self):
-        '''Initial load of layout/objects'''
-        self.series = classes.loadSeries('/home/michaelm/Documents/Test Series/test/ser1/BBCHZ.ser')
-        self.objects = classes.rObjectsFromSeries(self.series)
-
-        self.initFilterLine()
-        self.loadTable(rows=len(self.objects),cols=1)
+        self.initObjects()
         self.initLayout()
+        
         self.show()
         
-    def initFilterLine(self):
-        '''Loads filter input line and refresh button'''
-        self.filterLine = QtGui.QLineEdit(self)
-        self.filterLine.setText('Enter filters, separated by commas')
-        self.refreshButton = QtGui.QPushButton('Refresh')
-        self.refreshButton.clicked.connect( self.refresh )
-    
-    def loadTable(self, rows, cols):
-        '''Creates a QTableWidget with specified parameters'''
-        self.table = QtGui.QTableWidget(len(self.objects), cols, self)
+    def initObjects(self):
+        self.filter = QtGui.QLineEdit()
+        self.filter.setText('Enter filters, sep. by commas')
         
-        for object in range(len(self.objects)):
-            item = QtGui.QTableWidgetItem(str(self.objects[object]))
-            self.table.setItem(1,0,item)
+        self.refBut = QtGui.QPushButton()
+        self.refBut.setText('Refresh')
+        self.refBut.clicked.connect( self.ref )
         
-        self.table.verticalHeader().setVisible(False) # No row numbers
-        self.table.setHorizontalHeaderLabels(['Objects'])
-        self.table.resizeRowsToContents() # Reduces item row height to minimum         
-        self.table.horizontalHeader().setResizeMode(QtGui.QHeaderView.Stretch) # Keeps columns at width of window
+        self.objList = QtGui.QListWidget()
+        
+        self.addSerBut = QtGui.QPushButton()
+        self.addSerBut.setText('Add Series')
+        self.addSerBut.clicked.connect( self.addSer )
+        
+        self.delSerBut = QtGui.QPushButton()
+        self.delSerBut.setText('Remove Series')
+        self.delSerBut.clicked.connect( self.delSer )
         
     def initLayout(self):
-        filterLineBox = QtGui.QHBoxLayout()
-        filterLineBox.addWidget(self.filterLine)
-        filterLineBox.addWidget(self.refreshButton)
+        widgetLayout = QtGui.QVBoxLayout()
         
-        vbox = QtGui.QVBoxLayout()
-        vbox.addLayout(filterLineBox)
-        vbox.addWidget(self.table)
+        filterBox = QtGui.QHBoxLayout()
+        filterBox.addWidget( self.filter )
+        filterBox.addWidget( self.refBut )
+        widgetLayout.addLayout(filterBox)
         
-        self.setLayout(vbox)
-            
-    def refresh(self):
-        # Reload table with new specs
-        try:
-            self.objects = classes.rObjectsFromSeries(self.series)
-            self.loadTable(rows=len(self.objects), cols=1)
-            self.initUI()
-            print( 'refreshing table') #===
-        except:
-            print ('No series file loaded')
+        listBox = QtGui.QHBoxLayout()
+        listBox.addWidget( self.objList )
+        widgetLayout.addLayout(listBox)
+        
+        modListBox = QtGui.QHBoxLayout()
+        modListBox.addWidget( self.addSerBut )
+        modListBox.addWidget( self.delSerBut )
+        widgetLayout.addLayout(modListBox)
+        
+        self.setLayout(widgetLayout)
+    
+    def ref(self):
+        print('ref')
+    def addSer(self):
+        print('add ser')
+    def delSer(self):
+        print('del ser')
+        
+#===== Below is pre-ListWidget        
+#     def initFilterLine(self):
+#         '''Loads filter input line and refresh button'''
+#         self.filterLine = QtGui.QLineEdit(self)
+#         self.filterLine.setText('Enter filters, separated by commas')
+#         self.refreshButton = QtGui.QPushButton('Refresh')
+#         self.refreshButton.clicked.connect( self.refresh )
+#         self.addSeriesButton = QtGui.QPushButton('Add Series')
+#         self.addSeriesButton.clicked.connect( self.addSeries )
+#         self.removeSeriesButton = QtGui.QPushButton('Remove Series')
+#         self.removeSeriesButton.clicked.connect( self.remSeries )
+#     
+#     def loadList(self, rows, cols):
+#         '''Creates a QListWidget with specified parameters'''
+#         self.oList = QtGui.QListWidget(len(self.objects), cols, parent=self)
+#         
+#         for object in range(len(self.objects)):
+#             item = QtGui.QListWidgetItem(str(self.objects[object]))
+#             print(object)
+#             self.oList.setItem(object,0,item)
+#         
+#         self.oList.verticalHeader().setVisible(False) # No row numbers
+#         self.oList.setHorizontalHeaderLabels(['Objects'])
+#         self.oList.resizeRowsToContents() # Reduces item row height to minimum         
+#         self.oList.horizontalHeader().setResizeMode(QtGui.QHeaderView.Stretch) # Keeps columns at width of window
+#         
+#     def initLayout(self):
+#         filterLineBox = QtGui.QHBoxLayout()
+#         filterLineBox.addWidget(self.filterLine)
+#         filterLineBox.addWidget(self.refreshButton)
+#         
+#         bottomButBox = QtGui.QHBoxLayout()
+#         bottomButBox.addWidget(self.addSeriesButton)
+#         bottomButBox.addWidget(self.removeSeriesButton)
+#         
+#         vbox = QtGui.QVBoxLayout()
+#         vbox.addLayout(filterLineBox)
+#         vbox.addWidget(self.oList)
+#         vbox.addLayout(bottomButBox)
+#         
+#         self.setLayout(vbox)
+#     
+#     def addSeries(self): #===
+#         print 'add series'
+#     def remSeries(self): #===
+#         print 'rem series'
+#     def refresh(self):
+#         # Reload list with new specs
+#         try:
+#             self.oList.clearContents()
+#             self.initUI()
+#             print('refreshing list') #===
+#         except:
+#             print ('No series file loaded')
         
 app = QtGui.QApplication(sys.argv)
 objL = ObjectListWidget(width=300, height=800)
