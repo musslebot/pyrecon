3d5cce91b06112bcadab84547e361356f56d98b0
Michael Musslewhite <mdmusslewhite@gmail.com>
2013-11-06 19:19:43 -0600

    Update setup.py
    prep for 1.3.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ except ImportError:
 
 setup(
       name='PyRECONSTRUCT',
-      version='1.3.0',
+      version='1.3.1',
       author='Michael Musslewhite',
       author_email='mdmusslewhite@gmail.com',
       url='https://github.com/wtrdrnkr/pyrecon',
