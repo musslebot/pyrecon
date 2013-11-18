f01580643b18ecfc0bc038243477d766917f4937
wtrdrnkr <mdmusslewhite@gmail.com>
2013-11-18 17:14:36 -0600

    prep for 1.3.4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ except ImportError:
 
 setup(
       name='PyRECONSTRUCT',
-      version='1.3.3',
+      version='1.3.4',
       author='Michael Musslewhite',
       author_email='mdmusslewhite@gmail.com',
       url='https://github.com/wtrdrnkr/pyrecon',
