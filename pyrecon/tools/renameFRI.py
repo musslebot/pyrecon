f8476591ec94cb0bdcdb7802a782fd01f19fe1b0
wtrdrnkr <mdmusslewhite@gmail.com>
2013-11-19 17:28:37 -0600

    more dev for renameFRI

--- pyrecon/tools/renameFRI.py
+++ pyrecon/tools/renameFRI.py
@@ -6,63 +6,85 @@ parser.add_argument('Secondary Series', metavar='secondary', nargs=1, type=str, 
 parser.add_argument('Overlap Section', metavar='section', nargs=1, type=int, help='[integer] Section number with overlapping objects')
 parser.add_argument('Save Path', metavar='savePath', nargs=1, type=str, default='./renameFRI/', help='[string] Save path for the output (default: ./renameFRI/)')
 args = vars(parser.parse_args())  
+from lxml import etree as ET
 from pyrecon.tools import classes, mergeTool
-print(classes.__file__)
-print(mergeTool.__file__)
 
 # Set up object filters (regex and threshold)
+initials = re.compile('.{0,}_[a-z]{2}$', re.IGNORECASE)
 ax = re.compile('a[0-9]{1,}_[a-z]{2}$', re.IGNORECASE)
 dend = re.compile('d[0-9]{1,}_[a-z]{2}$', re.IGNORECASE)
-prot = re.compile('d[0-9]{1,}p[0-9]{2}', re.IGNORECASE)
-threshold=1.3
+cfa = re.compile('d[0-9]{1,}cfa[0-9]{1,}.{1,}_[a-z]{2}', re.IGNORECASE)
+c = re.compile('d[0-9]{1,}c[0-9]{1,}.{1,}_[a-z]{2}', re.IGNORECASE)
+astro = re.compile('astro_[a-z]{2}', re.IGNORECASE)
+threshold=(1+3**(-1))
 
 def main(primarySerPath, secondarySerPath, ovlpingSec, savePath='./renameFRI/'):
     # Load complete series from XML files
     series1 = classes.loadSeries(primarySerPath)
     series2 = classes.loadSeries(secondarySerPath)
     
+    # Remove all objects that don't end with initials
+    series1 = getRidOfMess(series1) 
+    series2 = getRidOfMess(series2)
+    
     # Extract Contours (traces/stamps/etc.) from the overlapping section (ovlpingSec) 
     conts1, conts2 = getContours(series1, series2, ovlpingSec) # get sets of contours in ovlpingSec
+    
+    # Extract specific traces from conts1, conts2
     ax1, ax2 = getAxons(conts1, conts2) # filter axons
     den1, den2 = getDendrites(conts1, conts2) # filter dendrites
-    pro1, pro2 = getProtrusions(conts1, conts2) # filter protrusions
+    cfa1, cfa2 = getCFAs(conts1, conts2)
+    c1, c2 = getCs(conts1, conts2)
+
+    # Get overlaps
+    a = checkOverlappingContours(ax1, ax2, threshold, sameName=False)
+    a = separateOverlappingContours(a[0], a[1], threshold, False)
+    mAxons = a[0] # matching axon pairs
     
-    #=== ADJUST THRESHOLD FOR chkOvlpConts and sepOvlpConts
-    # Check overlapping contours
-    print('THRESHOLD: '+str(threshold))
-    print('ovlp axons')
-    a = mergeTool.checkOverlappingContours(ax1, ax2, threshold=(1.3), sameName=False)
-    a = mergeTool.separateOverlappingContours(a[0], a[1], threshold=threshold, sameName=False)
-    print('Complete ovlps: ')
-    for group in a[0]:
-        print([thing.name for thing in group])
-    print('Conflict ovlps: ')
-    for group in a[1]:
-        print([thing.name for thing in group])
+    b = checkOverlappingContours(den1, den2, threshold, sameName=False)
+    b = separateOverlappingContours(b[0], b[1], threshold, sameName=False)
+    mDendrites = b[0] # matching dendrite pairs
     
-    print('ovlp dendrites')
-    b = mergeTool.checkOverlappingContours(den1, den2, threshold=threshold, sameName=False)
-    b = mergeTool.separateOverlappingContours(b[0], b[1], threshold=threshold, sameName=False)
-    print('Complete ovlps: ')
-    for group in b[0]:
-        print([thing.name for thing in group])
-    print('Conflict ovlps: ')
-    for group in b[1]:
-        print([thing.name for thing in group])
+    c = checkOverlappingContours(cfa1, cfa2, threshold, sameName=False)
+    c = separateOverlappingContours(c[0], c[1], threshold, sameName=False)
+    mCFAs = c[0] # matching CFA pairs
     
-    print('ovlp prots')
-    c = mergeTool.checkOverlappingContours(pro1, pro2, threshold=threshold, sameName=False)
-    c = mergeTool.separateOverlappingContours(c[0], c[1], threshold=threshold, sameName=False)
-    print('Complete ovlps: ')
-    for group in c[0]:
-        print([thing.name for thing in group])
-    print('Conflict ovlps: ')
-    for group in c[1]:
-        print([thing.name for thing in group])
+    d = checkOverlappingContours(c1, c2, threshold, sameName=False)
+    d = separateOverlappingContours(d[0], d[1], threshold, sameName=False)
+    mCs = d[0] # matching c pairs
+    
+    newSeries = makeEmptySeries(series1) # make empty series
+    # Populate sections with contours, *****except 104*****
+    for i in range(99,104): # Alicia traced these
+        newSeries.sections[i].contours = series1.sections[i].contours
+    for i in range(105, 110): # Nathaniel traced these
+        newSeries.sections[i].contours = series2.sections[i].contours
+    
+    # Remove initials from astrocytes
+    for section in newSeries.sections:
+        for contour in section.contours:
+            if astro.match(contour.name) != None:
+                contour.name = 'Astro'
+    
+    # Get overlapping objects on 104
+    axons = mergeAxons(mAxons)
+    dendrites = mergeDendrites(mDendrites)
+    cfas = mergeCFAs(mCFAs)
+    cs = mergeCs(mCs)
+    
+    # Rename objects according to overlaps found  
+    
+    # Print the new contour names for each section            
+    for section in newSeries.sections:
+        print section.name
+        print [cont.name for cont in section.contours]
    
-    # Print options
-    print('Renaming ovlping objects to match primary series based on section: '+str(ovlpingSec))
-    
+def getRidOfMess(series):
+    messlessSeries = makeEmptySeries(series)
+    for i in range(len(messlessSeries.sections)):
+        messlessSeries.sections[i].contours = [cont for cont in series.sections[i].contours if initials.match(cont.name)]
+    return messlessSeries
+
 def getContours(series1, series2, section):
     conts1 = [cont for cont in series1.sections[section].contours]
     conts2 = [cont for cont in series2.sections[section].contours]
@@ -88,15 +110,128 @@ def getDendrites(conts1, conts2):
     print('DONE')
     return dends1, dends2
 
-def getProtrusions(conts1, conts2):
-    '''Return a sorted lists of PROTRUSIONS for each of 2 contours lists (filter as stated at top of file)'''
-    print('Gathering protrusions...'),
-    prots1 = sorted([cont for cont in conts1 if prot.match(cont.name) != None],
+def getCFAs(conts1, conts2):
+    print('Gathering cfas...'),
+    cfa1 = sorted([cont for cont in conts1 if cfa.match(cont.name) != None],
                     key=lambda Contour: Contour.name)
-    prots2 = sorted([cont for cont in conts2 if prot.match(cont.name) != None],
+    cfa2 = sorted([cont for cont in conts2 if cfa.match(cont.name) != None],
                     key=lambda Contour: Contour.name)
     print('DONE')
-    return prots1, prots2
+    return cfa1, cfa2
+
+def getCs(conts1, conts2):
+    print('Gathering cs...'),
+    c1 = sorted([cont for cont in conts1 if c.match(cont.name) != None],
+                    key=lambda Contour: Contour.name)
+    c2 = sorted([cont for cont in conts2 if c.match(cont.name) != None],
+                    key=lambda Contour: Contour.name)
+    print('DONE')
+    return c1, c2
+
+def getAstros(conts1, conts2):
+    print('Gathering astrocytes...'),
+    astro1 = sorted([cont for cont in conts1 if astro.match(cont.name) != None],
+                    key=lambda Contour: Contour.name)
+    astro2 = sorted([cont for cont in conts2 if astro.match(cont.name) != None],
+                    key=lambda Contour: Contour.name)
+    print('DONE')
+    return astro1, astro2
+
+def checkOverlappingContours(contsA, contsB, threshold=(1+2**(-17)), sameName=False):
+    '''Returns lists of mutually overlapping contours.''' 
+    ovlpsA = [] # Section A contours that have overlaps in section B
+    ovlpsB = [] # Section B contours that have overlaps in section A
+    for contA in contsA:
+        ovlpA = []
+        ovlpB = []
+        for contB in contsB:
+            if sameName and contA.name == contB.name and contA.overlaps(contB, threshold) != 0:
+                ovlpA.append(contA)
+                ovlpB.append(contB)
+            elif not sameName and contA.overlaps(contB, threshold) != 0:
+                ovlpA.append(contA)
+                ovlpB.append(contB)
+        ovlpsA.extend(ovlpA)
+        ovlpsB.extend(ovlpB)
+    # Remove all non-unique contours from contsA and contsB
+    for cont in ovlpsA:
+        if cont in contsA:
+            contsA.remove(cont)
+    for cont in ovlpsB:
+        if cont in contsB:
+            contsB.remove(cont)
+    return ovlpsA, ovlpsB
+
+def separateOverlappingContours(ovlpsA, ovlpsB, threshold=(1+2**(-17)), sameName=False):
+    '''Returns a list of completely overlapping pairs and a list of conflicting overlapping pairs.'''
+    compOvlps = [] # list of completely overlapping contour pairs
+    confOvlps = [] # list of conflicting overlapping contour pairs
+    # Check for COMPLETELY overlapping contours 1st
+    for contA in ovlpsA:
+        for contB in ovlpsB:
+            if sameName and contA.name == contB.name:
+                if contA.overlaps(contB, threshold) == 1:
+                    compOvlps.append([contA, contB])
+            elif not sameName:
+                if contA.overlaps(contB, threshold) == 1:
+                    if [contA, contB] not in compOvlps:
+                        compOvlps.append([contA, contB])
+    # Check for CONFLICTING overlapping contours
+    for contA in ovlpsA:
+        for contB in ovlpsB:
+            if sameName and contA.name == contB.name:
+                overlap = contA.overlaps(contB, threshold)
+                if overlap != 0 and overlap != 1:
+                    confOvlps.append([contA, contB])
+            elif not sameName:
+                overlap = contA.overlaps(contB, threshold)
+                if overlap != 0 and overlap != 1:
+                    confOvlps.append([contA, contB])
+    return compOvlps, confOvlps
+
+def makeEmptySeries(primarySeries):
+    # Create .ser file based on primarySeries
+    mergedSeries = classes.Series( root=ET.Element('Series',primarySeries.output()[0]), name=primarySeries.name )
+    mergedSeries.contours = primarySeries.contours # Copy over primarySeries' Contours and ZContours
+    
+    # Create element tree Section element
+    for section in primarySeries.sections:
+        elem = ET.Element('Section')
+        for att in section.output():
+            elem.set(str(att), section.output()[att])
+        else:
+            name = mergedSeries.name+'.'+str(section.index)
+        sec3 = classes.Section(elem, name)
+        mergedSeries.sections.append(sec3)
+    return mergedSeries
+
+def mergeAxons(mAxons):
+    checkMatches(mAxons)
+    return
+
+def mergeDendrites(mDendrites):
+    checkMatches(mDendrites)
+    return
+
+def mergeCFAs(mCFAs):
+    checkMatches(mCFAs)
+    return
+
+def mergeCs(mCs):
+    checkMatches(mCs)
+    return
+
+def checkMatches(matchList):
+    print('The following objects will be matched: ')
+    for a,b in matchList:
+        print a.name+' == '+b.name
+    ans = raw_input('Is this okay (y/n)? ')
+    if str(ans) == 'y':
+        print('Okay')
+        print
+    else:
+        print('Aborting...')
+        quit()
 
 if __name__ == '__main__':
     print ('name == __main__')
