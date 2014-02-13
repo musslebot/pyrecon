d7f76b5ec82ddffe89b6832922c12067cee4bbc7
wtrdrnkr <mdmusslewhite@gmail.com>
2014-02-13 15:40:32 -0600

    moved 'mergeTool ready' print statement to after imports

--- pyrecon/mergeTool/__init__.py
+++ pyrecon/mergeTool/__init__.py
@@ -1,4 +1,4 @@
-print('mergeTool ready!')
 import merge # Driver for merging 2 series INCLUDING their sections
 import sectionMerge # Merge a single section
 import seriesMerge # Merge a series (.ser, not including sections)
+print('mergeTool ready!')
