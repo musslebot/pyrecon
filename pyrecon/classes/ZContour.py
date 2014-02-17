d6aa6b67fc6559eb6ea031f3385ab740fe2a3cd6
wtrdrnkr <mdmusslewhite@gmail.com>
2014-02-17 12:26:17 -0600

    added overlaps function

--- pyrecon/classes/ZContour.py
+++ pyrecon/classes/ZContour.py
@@ -28,3 +28,26 @@ class ZContour:
                     # Dict:attributes
                     if key in self.__dict__:
                         self.__dict__[key] = arg[key]
+    # mergeTool Functions
+    def overlaps(self, other, threshold=(1+2**(-17))):
+        def distance(pt0, pt1):
+            '''Distance formula: return distance between two points.'''
+            return math.sqrt( (pt0[0] - pt1[0])**2 + (pt0[1] - pt1[1])**2 )
+        # Check equal # pts
+        if len(self.points) != len(other.points):
+            return 0
+        # Build list of min distance between pts
+        distlist = []
+        for pt in self.points:
+            ptdistances = []
+            for pt2 in other.points:
+                if pt[2] == pt2[2]: # if in same section
+                    dist = distance(pt[0:2],pt[0:2])
+                    ptdistances.append( dist )
+            if len(ptdistances) != 0:
+                distlist.append( min(ptdistances) )
+        # check for any distances above threshold
+        for dist in distlist:
+            if dist > threshold: # no matching point
+                return 0
+        return 1  
