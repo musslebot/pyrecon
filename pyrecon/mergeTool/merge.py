ab3732ba0674c660a74819b0263fa51ab6f94f46
wtrdrnkr <mdmusslewhite@gmail.com>
2014-02-17 11:34:03 -0600

    main function (sans series stuff) complete

--- pyrecon/mergeTool/merge.py
+++ pyrecon/mergeTool/merge.py
@@ -2,25 +2,40 @@
 #!/usr/bin/python
 from pyrecon.main import openSeries
 from pyrecon.classes import Section, Series
+from pyrecon.mergeTool import sectionMerge, seriesMerge
+import pyrecon.handleXML as xml
 import argparse
 
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser(description='Merge two series together.')
 	parser.add_argument('input1', nargs=1, type=str, help='Path to the first series or directory')
 	parser.add_argument('input2', nargs=1, type=str, help='Path to the second series or directory')
-	parser.add_argument('output', nargs=1, type=str, help='Path to the directory for writing the merged series\' XML files')
+	parser.add_argument('outpath', nargs=1, type=str, help='Path to the directory for writing the merged series\' XML files')
 	args = vars(parser.parse_args())
 	# Assign argparse things to their variables
 	series1 = openSeries( str(args['input1'][0]) )
 	series2 = openSeries( str(args['input2'][0]) )
-	directory = str(args['output'][0])
+	directory = str(args['outpath'][0])
 	main(series1, series2, directory)
 
-def main(series1, series2, directory):
-	print series1
-	print series2
-	print directory
+def main(series1, series2, directory, *args, **kwargs):
+	# GUI handlers?
+	gui = False
+	if 'graphical' in args or kwargs['graphical'] == True:
+		gui = True
+	# Copy series1 attributes, etc. for now #===
+	mergedSer = Series(series1.__dict__) #===
+	mergeSer.sections = [] #===
+	# Merge sections
+	allSections = zip(series1.sections, series2.sections)
+	for secPair in allSections:
+		print secPair[0].name+' '+secPair[1].name
+		print str(secPair)
+		mergedSec = sectionMerge.main(*secPair, graphical=gui)
+		mergedSer.sections.append(mergedSec)
+	# Write <series> & <sections> to XML files in directory
+	xml.writeSeries(mergeSer, directory, sections=True)
 
-class mergeObject:
-	def __init__(self, *args, **kwargs):
-		return
+# class mergeObject:
+	# def __init__(self, *args, **kwargs):
+		# return
