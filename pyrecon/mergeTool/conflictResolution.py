0f9230cc42099164a0654fd478e8252fce315618
wtrdrnkr <mdmusslewhite@gmail.com>
2014-02-17 12:45:50 -0600

    finished basic mergeZContours and low priority placeholders for contours and attributes

--- pyrecon/mergeTool/conflictResolution.py
+++ pyrecon/mergeTool/conflictResolution.py
@@ -76,14 +76,20 @@ def sectionAttributes(dictA, dictB):
 				else:
 					print('Invalid entry, try again.')
 	return outputAttributes
-
-# SERIES #===
+# SERIES
 # - Contours
-def seriesContours(contsA, contsB): #===
-	return
+def seriesContours(contsA, contsB): #=== low priority, return A's contours
+	return contsA
 # - ZContours
-def seriesZContours(zcontsA, zcontsB): #===
-	return
+def seriesZContours(ser1zconts, ser2zconts, ser3zconts): #=== HIGH PRIORITY
+	# add leftover, unique zcontours to ser3zconts
+	ser3zconts.extend(ser1zconts)
+	ser3zconts.extend(ser2zconts)
+	return ser3zconts
 # - Attributes
-def seriesAttributes(dictA, dictB): #===
-	return
+def seriesAttributes(dictA, dictB): #=== low priority, return A's attributes
+	mergedAttributes = {}
+	for key in dictA:
+		if key not in ['zcontours','contours', 'sections']: # ignore zcontours, contours, sections -- they have their own merge functions
+			mergedAttributes[key] = dictA[key]
+	return mergedAttributes
