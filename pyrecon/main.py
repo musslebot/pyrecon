9b505432f7c786f1399745d329cbd1ec0012a2f0
wtrdrnkr <mdmusslewhite@gmail.com>
2014-03-12 13:16:30 -0500

    finished toolLoader and tested for all tools

--- pyrecon/main.py
+++ pyrecon/main.py
@@ -1,6 +1,6 @@
 '''Main, overarching functions.'''
 from pyrecon.classes import Section, Series
-import os, re
+import os
 try:
 	from PySide.QtCore import *
 	from PySide.QtGui import *
@@ -20,19 +20,6 @@ def openSeries(path):
 	series = Series(pathToSeries)
 	series.update(sections=True) # find sections in directory
 	return series
-
-def merge(path1, path2, outputDirectory, *args, **kwargs): #===
-	# direct arguments to mergeTool.merge; arg handling is performed there
-	return 
-
-def curate(series, thresholdForDistantTraces): #===
-	return
-
-def excel(series, outputDirectory): #===
-	return
-
-def calibrate(path): #===
-	return
 
 def start():
 	app = QApplication.instance()
@@ -55,25 +42,38 @@ class toolLoader(QWidget):
 		self.calibBut = QPushButton('calibrationTool')
 	def loadFunctions(self):
 		self.mergeBut.clicked.connect( self.mergeGo )
+		self.mergeBut.setToolTip('Merge two series together')
 		self.excelBut.clicked.connect( self.excelGo )
+		self.excelBut.setToolTip('Output series data to an excel file (.xlsx)')
 		self.curateBut.clicked.connect( self.curateGo )
+		self.curateBut.setToolTip('Automatically search through a series for special traces: duplicates, distant, reverse')
 		self.calibBut.clicked.connect( self.calibGo )
+		self.calibBut.setToolTip('Calibrate a series to match image')
 	def loadLayouts(self):
 		vbox = QVBoxLayout()
-		vbox.addWidget(self.mergeBut)
-		vbox.addWidget(self.excelBut)
-		vbox.addWidget(self.curateBut)
-		vbox.addWidget(self.calibBut)
+		hbox1 = QHBoxLayout()
+		hbox1.addWidget(self.mergeBut)
+		hbox2 = QHBoxLayout()
+		hbox2.addWidget(self.excelBut)
+		hbox3 = QHBoxLayout()
+		hbox3.addWidget(self.curateBut)
+		hbox4 = QHBoxLayout()
+		hbox4.addWidget(self.calibBut)
+		for lay in [hbox1, hbox2, hbox3, hbox4]:
+			vbox.addLayout(lay)
 		self.setLayout(vbox)
 	def mergeGo(self):
 		self.a = mergeToolLoader()
 		self.close()
-	def excelGo(self): #===
-		print('excelTool')
-	def curateGo(self): #===
-		print('curationTool')
-	def calibGo(self): #===
-		print('calibrationTool')
+	def excelGo(self):
+		self.a = excelToolLoader()
+		self.close()
+	def curateGo(self):
+		self.a = curationToolLoader()
+		self.close()
+	def calibGo(self):
+		self.a = calibrationToolLoader()
+		self.close()
 
 class mergeToolLoader(QWidget):
 	def __init__(self, title='mergeTool Loader'):
@@ -135,6 +135,142 @@ class mergeToolLoader(QWidget):
 		self.close()
 		# Run mergeTool
 		mergeTool.merge.main(*self.output, graphical=True)
-		
 
+class excelToolLoader(QWidget):
+	def __init__(self, title='excelTool Loader'):
+		QWidget.__init__(self)
+		self.setWindowTitle(title)
+		self.loadObjects()
+		self.loadFunctions()
+		self.loadLayouts()
+		self.show()
+	def loadObjects(self):
+		self.path = QLineEdit(self)
+		self.path.setText('<Enter path to series, or browse>')
+		self.outDir = QLineEdit(self)
+		self.outDir.setText('<Enter directory to save .xlsx file>')
+		self.browse = QPushButton(self)
+		self.browse.setText('Browse')
+		self.browse2 = QPushButton(self)
+		self.browse2.setText('Browse')
+		self.doneBut = QPushButton(self)
+		self.doneBut.setText('Begin excelTool')
+	def loadFunctions(self):
+		self.browse.clicked.connect( self.browseStuff )
+		self.browse2.clicked.connect( self.browseStuff )
+		self.doneBut.clicked.connect( self.finish )
+	def loadLayouts(self):
+		vbox = QVBoxLayout()
+		hbox = QHBoxLayout()
+		hbox.addWidget(self.path)
+		hbox.addWidget(self.browse)
+		hbox2 = QHBoxLayout()
+		hbox2.addWidget(self.outDir)
+		hbox2.addWidget(self.browse2)
+		vbox.addLayout(hbox)
+		vbox.addLayout(hbox2)
+		vbox.addWidget(self.doneBut)
+		self.setLayout(vbox)
+	def browseStuff(self):
+		if self.sender() == self.browse:
+			fileName = QFileDialog.getOpenFileName(self, "Load Series 1", "/home/", "Series File (*.ser)")
+			self.path.setText( str(fileName[0]) )
+		elif self.sender() == self.browse2:
+			dirName = QFileDialog.getExistingDirectory(self)
+			self.outDir.setText( str(dirName) )
+	def finish(self):
+		from pyrecon import excelTool
+		self.output = ( str(self.path.text()), str(self.outDir.text()) )
+		self.close()
+		excelTool.excelTool.main(*self.output)
 
+class curationToolLoader(QWidget):
+	def __init__(self, title='curationTool Loader'):
+		QWidget.__init__(self)
+		self.setWindowTitle(title)
+		self.loadObjects()
+		self.loadFunctions()
+		self.loadLayouts()
+		self.show()
+	def loadObjects(self):
+		self.path = QLineEdit(self)
+		self.path.setText('<Enter path to series, or browse>')
+		self.distance = QLineEdit(self)
+		self.distance.setText('<Enter distance threshold (integer)>')
+		self.browse = QPushButton(self)
+		self.browse.setText('Browse')
+		self.doneBut = QPushButton(self)
+		self.doneBut.setText('Begin curationTool')
+	def loadFunctions(self):
+		self.browse.clicked.connect( self.browseStuff )
+		self.doneBut.clicked.connect( self.finish )
+	def loadLayouts(self):
+		vbox = QVBoxLayout()
+		hbox = QHBoxLayout()
+		hbox.addWidget(self.path)
+		hbox.addWidget(self.browse)
+		hbox2 = QHBoxLayout()
+		hbox2.addWidget(self.distance)
+		vbox.addLayout(hbox)
+		vbox.addLayout(hbox2)
+		vbox.addWidget(self.doneBut)
+		self.setLayout(vbox)
+	def browseStuff(self):
+		if self.sender() == self.browse:
+			fileName = QFileDialog.getOpenFileName(self, "Load Series", "/home/", "Series File (*.ser)")
+			self.path.setText( str(fileName[0]) )
+	def finish(self):
+		from pyrecon import curationTool
+		# Add paths to self.output
+		self.output = ( str(self.path.text()), int(self.distance.text()) )
+		self.close()
+		# Run mergeTool
+		curationTool.curationTool.main(*self.output)
+
+class calibrationToolLoader(QWidget):
+	def __init__(self, title='calibrationTool Loader'):
+		QWidget.__init__(self)
+		self.setWindowTitle(title)
+		self.loadObjects()
+		self.loadFunctions()
+		self.loadLayouts()
+		self.show()
+	def loadObjects(self):
+		self.path1 = QLineEdit(self)
+		self.path1.setText('<Enter path to series, or browse>')
+		self.browse1 = QPushButton(self)
+		self.browse1.setText('Browse')
+		self.outDir = QLineEdit(self)
+		self.outDir.setText('<Enter directory to save calibrated series in>')
+		self.browse2 = QPushButton(self)
+		self.browse2.setText('Browse')
+		self.doneBut = QPushButton(self)
+		self.doneBut.setText('Begin calibrationTool')
+	def loadFunctions(self):
+		self.browse1.clicked.connect( self.browseStuff )
+		self.browse2.clicked.connect( self.browseStuff )
+		self.doneBut.clicked.connect( self.finish )
+	def loadLayouts(self):
+		vbox = QVBoxLayout()
+		hbox1 = QHBoxLayout()
+		hbox1.addWidget(self.path1)
+		hbox1.addWidget(self.browse1)
+		vbox.addLayout(hbox1)
+		hbox2 = QHBoxLayout()
+		hbox2.addWidget(self.outDir)
+		hbox2.addWidget(self.browse2)
+		vbox.addLayout(hbox2)
+		vbox.addWidget(self.doneBut)
+		self.setLayout(vbox)
+	def browseStuff(self):
+		if self.sender() == self.browse1:
+			fileName = QFileDialog.getOpenFileName(self, "Load Series", "/home/", "Series File (*.ser)")
+			self.path1.setText( str(fileName[0]) )
+		elif self.sender() == self.browse2:
+			dirName = QFileDialog.getExistingDirectory(self)
+			self.outDir.setText( str(dirName) )
+	def finish(self):
+		from pyrecon import calibrationTool
+		self.output = ( str(self.path1.text()), str(self.outDir.text()) )
+		self.close()
+		calibrationTool.reScale.main(self.output[0], calibrationTool.findCalFactor.findCalFactor(self.output[0]), self.output[1])
