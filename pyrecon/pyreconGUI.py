a6f0a09a42634c45b028c1ff47b8481724ba7569
wtrdrnkr <mdmusslewhite@gmail.com>
2014-03-20 11:51:38 -0500

    fixed imports, removed tests, fixed parent issues

--- pyrecon/pyreconGUI.py
+++ pyrecon/pyreconGUI.py
@@ -2,6 +2,7 @@
 from PySide.QtCore import *
 from PySide.QtGui import *
 import pyrecon
+# Pyrecon tool modules imported when called by their functions below
 
 class pyreconMainWindow(QMainWindow):
     '''Main PyRECONSTRUCT window.'''
@@ -25,15 +26,6 @@ class pyreconMainWindow(QMainWindow):
         self.fileMenu.addAction( saveAction )
     def loadToolsMenu(self):
         # 1) Create Actions
-        
-        #===
-        # - TEST
-        testAction = QAction( QIcon(), 'TEST', self )
-        testAction.triggered.connect( self.test )
-        testAction.setStatusTip('TEST HERE')
-        self.toolsMenu.addAction( testAction )
-        #===
-
         # - mergeTool
         mergeAction = QAction( QIcon(), 'mergeTool', self ) #QIcon() is null, but necessary for Action creation
         mergeAction.triggered.connect( self.loadMergeTool )
@@ -55,28 +47,18 @@ class pyreconMainWindow(QMainWindow):
         self.toolsMenu.addAction( calibAction )
         self.toolsMenu.addAction( excelAction )
         self.toolsMenu.addAction( curateAction )
-    def loadMergeTool(self): #===
-        from pyrecon.mergeTool.gui.main import mergeSelection
-        self.lDock = QDockWidget() # Left dockWidget
-        mergeSel = mergeSelection() #=== parent stuff? Whats the deal? i.e. print statements not working in mergeSelection
-        self.lDock.setWidget( mergeSel )
-        self.addDockWidget( Qt.LeftDockWidgetArea, self.lDock )
+    def loadMergeTool(self):
+        from pyrecon.mergeTool.gui.mergeGUI import mergeSelection
+        self.mergeSelector = QDockWidget() # Left dockWidget
+        mergeSel = mergeSelection(self)
+        self.mergeSelector.setWidget( mergeSel )
+        self.addDockWidget( Qt.LeftDockWidgetArea, self.mergeSelector )
         # PlaceHolder for resoution widgets
         self.placeHolder = QLabel()
         self.placeHolder.setPixmap( QPixmap(750,750) )
         self.placeHolder.setAlignment( Qt.AlignCenter )
         self.setCentralWidget( self.placeHolder )
-    #===
-    def test(self):
-        from pyrecon.mergeTool.gui.mergeGUI import testWidget2
-        print('Begin Test')
-        lDock = QDockWidget() # Widget on same page as mainWindow
-        rDock = QDockWidget() # Widget in mergeTool/gui/mergeGUI
-        lDock.setWidget( testWidget() )
-        rDock.setWidget( testWidget2() )
-        self.addDockWidget( Qt.LeftDockWidgetArea, lDock)
-        self.addDockWidget( Qt.RightDockWidgetArea, rDock)
-    #===
+
     def loadCalib(self): #===
         print('Load calibration widget')
     def loadExcel(self): #===
@@ -85,38 +67,6 @@ class pyreconMainWindow(QMainWindow):
         print('Load curation widget')
     def save(self): #===
         print('THIS WILL SAVE YOUR SERIES (in the future :D)')
-
-#===
-class testWidget(QWidget):
-    def __init__(self):
-        QWidget.__init__(self)
-        self.loadObjects()
-        self.loadFunctions()
-        self.loadLayout()
-    def loadObjects(self):
-        self.b1 = QPushButton()
-        self.b2 = QPushButton()
-        self.b3 = QPushButton()
-    def loadFunctions(self):
-        self.b1.setText('One')
-        self.b2.setText('Two')
-        self.b3.setText('Three')
-        for but in [self.b1, self.b2, self.b3]:
-            but.clicked.connect( self.butClick )
-    def loadLayout(self):
-        vbox = QVBoxLayout()
-        vbox.addWidget(self.b1)
-        vbox.addWidget(self.b2)
-        vbox.addWidget(self.b3)
-        self.setLayout(vbox)
-    def butClick(self):
-        if self.sender() == self.b1:
-            print('BUTTON 1')
-        elif self.sender() == self.b2:
-            print('BUTTON 2')
-        elif self.sender() == self.b3:
-            print('BUTTON 3')
-#===
 
 class browseWidget(QWidget):
     '''Provides a QLineEdit and button for browsing through a file system. browseType can be directory, file or series but defaults to directory.'''
