5f61c999c17aea3198e93b0a61447559b01751b4
wtrdrnkr <mdmusslewhite@gmail.com>
2014-03-28 16:12:29 -0500

    added arguments to main function to specify which fxns to run. printOut argument added to specify wheter to return print to stdout or to return a list of strings

--- pyrecon/curationTool/curationTool.py
+++ pyrecon/curationTool/curationTool.py
@@ -2,57 +2,98 @@
 from pyrecon.main import openSeries
 import argparse
 
-def main(seriesPath, threshold):
-	series = openSeries(seriesPath)
-	print('======================')
-	print('curationTool on %s')%series.name
-	print('======================')
-	findDuplicateTraces(series)
-	findDistantTraces(series, threshold)
-	findReverseTraces(series)
-
-def findDuplicateTraces(series):
+def main(series, threshold, duplicates=True, distant=True, reverse=True, printOut=True):
+	if type(series) == type(''):
+		series = openSeries(series)
+	if printOut:
+		print('======================')
+		print('curationTool on %s')%series.name
+		print('======================')
+	a=''
+	b=''
+	c=''
+	if duplicates:
+		a = findDuplicateTraces(series, printOut=printOut)
+	if distant:
+		b = findDistantTraces(series, threshold, printOut=printOut)
+	if reverse:
+		c = findReverseTraces(series, printOut=printOut)
+	if not printOut:
+		return (a,b,c)
+def findDuplicateTraces(series, printOut=True):
 	'''Prints the duplicates found within every section of <series>'''
-	print('--------------------------')
-	print('Locating duplicate traces:')
-	print('--------------------------')
 	if type(series) == type(''):
 		series = openSeries(series)
 	duplicateDict = series.locateDuplicates()
-	for sec in sorted(duplicateDict):
-		print('Section index: '+str(sec))
-		for name in list(set([cont.name for cont in duplicateDict[sec]])):
-			print('\t'+name)
-		print
+	if printOut:
+		print('--------------------------\n'+'Locating duplicate traces:'+'--------------------------\n')
+		for sec in sorted(duplicateDict):
+			print('Section index: '+str(sec))
+			for name in list(set([cont.name for cont in duplicateDict[sec]])):
+				print('\t'+name)
+			print
+		return
+	else:
+		output = []
+		output.append('--------------------------')
+		output.append('Locating duplicate traces:')
+		output.append('--------------------------')
+		for sec in sorted(duplicateDict):
+			output.append('Section index: '+str(sec))
+			for name in list(set([cont.name for cont in duplicateDict[sec]])):
+				output.append('\t'+name)
+			output.append('\n')
+		return output
 
-def findDistantTraces(series, threshold):
-	'''Prints traces of the same name separated by <threshold (default: 7)> sections that do not contain that section'''
-	print('-------------------------------------------------')
-	print('Locating distant traces (treshold: +/-%d sections):')%threshold
-	print('-------------------------------------------------')
+def findDistantTraces(series, threshold, printOut=True):
+	'''Prints traces of the same name separated by <threshold> sections that do not contain that section'''
 	if type(series) == type(''):
 		series = openSeries(series)
 	distantDict = series.locateDistantTraces(threshold)
-	for sec in sorted(distantDict):
-		print('Section index: '+str(sec))
-		for name in distantDict[sec]:
-			print('\t'+name)
-		print
+	if printOut:
+		print('-------------------------------------------------\n'+str('Locating distant traces (treshold: +/-%d sections):'%threshold)+'-------------------------------------------------\n')
+		for sec in sorted(distantDict):
+			print('Section index: '+str(sec))
+			for name in distantDict[sec]:
+				print('\t'+name)
+			print
+		return
+	else:
+		output = []
+		output.append('-------------------------------------------------')
+		output.append(str('Locating distant traces (treshold: +/-%d sections):'%threshold))
+		output.append('-------------------------------------------------')
+		for sec in sorted(distantDict):
+			output.append('Section index: '+str(sec))
+			for name in distantDict[sec]:
+				output.append('\t'+name)
+			output.append('\n')
+		return output
 
-def findReverseTraces(series):
+def findReverseTraces(series, printOut=True):
 	'''Prints all the reverse traces found in a series (per section)'''
-	print('------------------------')
-	print('Locating reverse traces:')
-	print('------------------------')
 	if type(series) == type(''):
 		series = openSeries(series)
 	reverseDict = series.locateReverseTraces()
-	for sec in sorted(reverseDict):
-		print('Section index: '+str(sec))
-		for cont in reverseDict[sec]:
-			print('\t'+cont.name)
-		print
-
+	if printOut:
+		print('------------------------\n'+'Locating reverse traces:'+'------------------------\n')
+		for sec in sorted(reverseDict):
+			print('Section index: '+str(sec))
+			for cont in reverseDict[sec]:
+				print('\t'+cont.name)
+			print
+		return
+	else:
+		output = []
+		output.append('------------------------')
+		output.append('Locating reverse traces:')
+		output.append('------------------------')
+		for sec in sorted(reverseDict):
+			output.append('Section index: '+str(sec))
+			for cont in reverseDict[sec]:
+				output.append('\t'+cont.name)
+			output.append('\n')
+		return output
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Performs various functions to assist curating a series')
     parser.add_argument('series', nargs=1, type=str, help='Path to the series/sections that needs to be curated')
