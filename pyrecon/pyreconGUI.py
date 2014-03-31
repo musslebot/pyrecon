be992c80e5a6523bee13bc8a5a73c71788e33190
wtrdrnkr <mdmusslewhite@gmail.com>
2014-03-31 14:03:45 -0500

    minor updates

--- pyrecon/pyreconGUI.py
+++ pyrecon/pyreconGUI.py
@@ -64,7 +64,6 @@ class pyreconMainWindow(QMainWindow):
     def loadExcelTool(self): #===
         print('Load excel widget')
     
-
 class browseWidget(QWidget):
     '''Provides a QLineEdit and button for browsing through a file system. browseType can be directory, file or series but defaults to directory.'''
     def __init__(self, browseType='directory'):
