f82ab21fdc8dec0f68257e45d98221797dc006b6
wtrdrnkr <mdmusslewhite@gmail.com>
2014-04-03 11:15:28 -0500

    moved gui stuff into gui folder, imports fixed and __init__ created

--- pyrecon/excelTool/gui/excelGUI.py
+++ pyrecon/excelTool/gui/excelGUI.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
 import sys
-import precon.excelTool
-from PySide import QtGui, QtCore
+from pyrecon.excelTool import excelTool
+from PySide.QtGui import *
+from PySide.QtCore import *
 
-class excelToolWindow(QtGui.QWidget):
+class excelToolWindow(QWidget):
     def __init__(self, parent = None):
-        QtGui.QWidget.__init__(self, parent)
+        QWidget.__init__(self, parent)
         self.parent = parent
         self.setGeometry(0,0,500,200)
         
@@ -25,44 +26,46 @@ class excelToolWindow(QtGui.QWidget):
         self.show()
         
     def functionalItems(self):
-        self.seriesPathLine = QtGui.QLineEdit(self)
+        self.seriesPathLine = QLineEdit(self)
         self.seriesPathLine.setText( self.seriesPath )
-        self.seriesPathLine.setAlignment( QtCore.Qt.AlignCenter )
+        self.seriesPathLine.setAlignment( Qt.AlignCenter )
         
-        self.seriesPathBrowse = QtGui.QPushButton(self)
+        self.seriesPathBrowse = QPushButton(self)
         self.seriesPathBrowse.clicked.connect( self.browse )
-        self.seriesPathBrowse.setIconSize(QtCore.QSize(25,25))
+        self.seriesPathBrowse.setIconSize(QSize(25,25))
         self.seriesPathBrowse.setText('Browse')
         
-        self.savePathLine = QtGui.QLineEdit(self)
+        self.savePathLine = QLineEdit(self)
         self.savePathLine.setText( self.savePath )
-        self.savePathLine.setAlignment( QtCore.Qt.AlignCenter ) #===
+        self.savePathLine.setAlignment( Qt.AlignCenter ) #===
         
-        self.savePathBrowse = QtGui.QPushButton(self)
+        self.savePathBrowse = QPushButton(self)
         self.savePathBrowse.clicked.connect( self.browse )
-        self.savePathBrowse.setIconSize(QtCore.QSize(25,25))
+        self.savePathBrowse.setIconSize(QSize(25,25))
         self.savePathBrowse.setText('Browse')
         
-        self.goButton = QtGui.QPushButton(self)
+        self.goButton = QPushButton(self)
         self.goButton.setText('Create Excel Workbook (.xlsx)')
         self.goButton.clicked.connect( self.checkAndFinish )
+        self.goButton.setMinimumHeight(50)
+        self.goButton.setFlat(True)
 
     def layout(self):
-        vbox = QtGui.QVBoxLayout()
+        vbox = QVBoxLayout()
         
-        hbox1 = QtGui.QHBoxLayout()
+        hbox1 = QHBoxLayout()
         hbox1.addWidget( self.seriesPathLine )
         hbox1.addWidget( self.seriesPathBrowse )
         hbox1.insertSpacing(0,25)
         hbox1.insertSpacing(-1,25)
         
-        hbox2 = QtGui.QHBoxLayout()
+        hbox2 = QHBoxLayout()
         hbox2.addWidget( self.savePathLine )
         hbox2.addWidget( self.savePathBrowse )
         hbox2.insertSpacing(0,25)
         hbox2.insertSpacing(-1,25)
         
-        hbox3 = QtGui.QHBoxLayout()
+        hbox3 = QHBoxLayout()
         hbox3.insertSpacing(0,225)
         hbox3.addWidget( self.goButton )
         hbox3.insertSpacing(-1,225)
@@ -74,7 +77,7 @@ class excelToolWindow(QtGui.QWidget):
 
     def browse(self):
             if self.sender() == self.seriesPathBrowse:
-                path = QtGui.QFileDialog.getOpenFileName(self,
+                path = QFileDialog.getOpenFileName(self,
                                                      'Load Series',
                                                      '/home/',
                                                      'Series File (*.ser)')
@@ -83,31 +86,22 @@ class excelToolWindow(QtGui.QWidget):
                     self.seriesPathLine.setText(path)
                 
             elif self.sender() == self.savePathBrowse:
-                path = str( QtGui.QFileDialog.getExistingDirectory(self) )
+                path = str( QFileDialog.getExistingDirectory(self) )
                 if path != '':
                     self.savePathLine.setText(path)
-                      
+                self.goButton.setFlat(False)
     def checkAndFinish(self):
         self.seriesPath = self.seriesPathLine.text()
         self.savePath = self.savePathLine.text()
         if '.ser' not in self.seriesPath:
-            msg = QtGui.QMessageBox(self)
+            msg = QMessageBox(self)
             msg.setText('Invalid series file -- Please try again.')
             msg.show()
         if self.savePath == 'Enter or browse path to save excel workbook' or '/' not in self.savePath:
-            msg = QtGui.QMessageBox(self)
+            msg = QMessageBox(self)
             msg.setText('Invalid save path!')
             msg.show()
         else:
-            print('Continuing...')
-            print(self.seriesPath)
-            print(self.savePath)
-            excelTool.main(self.seriesPath, self.savePath)
+            excelTool.main(str(self.seriesPath), str(self.savePath))
             self.close()
-            
-def main():
-    app = QtGui.QApplication(sys.argv)
-    t = excelToolWindow()
-    sys.exit( app.exec_() )
-main()
     
