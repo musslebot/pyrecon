b7087d9d6f0769214ddc6484f3d4a48d5a35eb7f
wtrdrnkr <mdmusslewhite@gmail.com>
2014-04-10 14:16:51 -0500

    new __init__.py files and updated pyrecon's main __init__ for new structure

--- pyrecon/__init__.py
+++ pyrecon/__init__.py
@@ -1,24 +1,11 @@
 __all__ = [
-	'calibrationTool',
 	'classes',
-	'curationTool',
-	'excelTool',
-	'mergeTool'
+	'tools',
+	'gui',
 ]
 
 # Important functions/classes & xml handling
 from classes import Contour, Image, Section, Series, Transform, ZContour
 from main import openSeries, start
-import handleXML as xml
-
-# Import packages!
-print('Initializing tools:')
-import mergeTool
-print('\tmergeTool ready!')
-import excelTool
-print('\texcelTool ready!')
-import curationTool
-print('\tcurationTool ready!')
-import calibrationTool
-print('\tcalibrationTool ready!')
+import ..tools.handleXML as xml
 
---
+++ pyrecon/gui/__init__.py
@@ -0,0 +1,4 @@
+import calibrationTool
+import curationTool
+import excelTool
+import mergeTool
