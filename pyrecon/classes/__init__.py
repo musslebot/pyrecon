c7ad0efbda19e77ebc8b5081c62b8e0da06adef0
wtrdrnkr <mdmusslewhite@gmail.com>
2014-04-11 15:14:21 -0500

    imports fixed

--- pyrecon/classes/__init__.py
+++ pyrecon/classes/__init__.py
@@ -1,5 +1,16 @@
+__all__ = [
+	'Contour',
+	'Image',
+	'MultiSectionContour',
+	'Section',
+	'Series',
+	'Transform',
+	'ZContour'
+	]
+
 from Contour import *
 from Image import *
+from MultiSectionContour import *
 from Section import *
 from Series import *
 from Transform import *
