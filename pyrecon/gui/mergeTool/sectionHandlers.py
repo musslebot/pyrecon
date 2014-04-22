475d247962b487d1ee2c34a60dbbc68ffaeeab4c
wtrdrnkr <mdmusslewhite@gmail.com>
2014-04-22 16:03:04 -0500

    finished SectionMergeWrapper and SectionContourHandler

--- pyrecon/gui/mergeTool/sectionHandlers.py
+++ pyrecon/gui/mergeTool/sectionHandlers.py
@@ -4,60 +4,24 @@ import pyrecon
 import numpy as np
 
 # SECTION RESOLUTION WRAPPER
-# class sectionWrapper(QTabWidget):
-# 	'''sectionWrapper is a TabWidget. It contains multiple widgets that can be swapped via their tabs.'''
-# 	def __init__(self, MergeSection=None, parent=None):
-# 		QTabWidget.__init__(self)
-# 		self.parent = parent
-# 		self.merge = MergeSection
-# 		self.loadObjects(MergeSection.section1, MergeSection.section2)
-# 	def loadObjects(self, section1, section2):
-# 		# Load widgest to be used as tabs
-# 		self.attributes = self.merge.mergeAttributes()
-# 		self.images = self.merge.mergeImages()
-# 		self.contours = self.merge.mergeContours()
-# 		# Add widgets as tabs
-# 		# - Attributes
-# 		self.addTab(self.attributes, '&Attributes')
-# 		# - Images
-# 		self.addTab(self.images, '&Images')
-# 		# - Contours
-# 		self.addTab(self.contours, '&Contours')
-# 		# Check for lack of conflicts
-# 		self.isResolved()
-# 	def toObject(self):
-# 		'''Returns a section object from the output of each resolution tab.'''
-# 		'''Returns series object from the output of each resolution tab.'''
-# 		# Determine attributes
-# 		if self.attributes.output is None:
-# 			print('Section attributes default to section 1')
-# 			attributes = self.attributes.atts1
-# 		else:
-# 			attributes = self.attributes.output
-# 		# Determine image
-# 		if self.images.output is None:
-# 			print('Section image default to section 1')
-# 			image = self.images.img1
-# 		else:
-# 			image = self.images.output
-# 		# Determine contours
-# 		if self.contours.output is None:
-# 			print('Section contours default to section 1')
-# 			contours = self.contours.conts1
-# 		else:
-# 			contours = self.contours.output
-		
-# 		# Create merged section object
-# 		return pyrecon.classes.Section(attributes,image,contours)
-# 	def isResolved(self):
-# 		'''Returns true if self.attributes/images/contours output attribute != None'''
-# 		resolved = (self.attributes.output is not None and
-# 					self.images.output is not None and
-# 					self.contours.output is not None)
-# 		# Check if parent is merge item and set bg to green
-# 		if resolved and self.parent.__class__.__name__ == 'mergeItem':
-# 			self.parent.setBackground(QColor('lightgreen'))
-# 		return resolved
+class SectionMergeWrapper(QTabWidget):
+	'''sectionWrapper is a TabWidget. It contains multiple widgets that can be swapped via their tabs.'''
+	def __init__(self, MergeSection):
+		QTabWidget.__init__(self)
+		self.merge = MergeSection
+		self.loadObjects()
+	def loadObjects(self):
+		# Load widgest to be used as tabs
+		self.attributes = SectionAttributeHandler(self.merge)
+		self.images = SectionImageHandler(self.merge)
+		self.contours = SectionContourHandler(self.merge)
+		# Add widgets as tabs
+		# - Attributes
+		self.addTab(self.attributes, '&Attributes')
+		# - Images
+		self.addTab(self.images, '&Images')
+		# - Contours
+		self.addTab(self.contours, '&Contours')
 # - Attributes
 class SectionAttributeHandler(QWidget):
 	def __init__(self, mergeObject):
@@ -183,123 +147,21 @@ class SectionImageHandler(QWidget):
 			self.merge.images = self.merge.section2.image
 			self.chooseLeft.setStyleSheet(QWidget().styleSheet())
 			self.chooseRight.setStyleSheet('background-color:lightgreen;')
-
-class sectionImages(QWidget):
-	def __init__(self, image1, image2, parent=None):
-		QWidget.__init__(self, parent)
-		self.parent = parent
-		self.img1 = image1
-		self.img2 = image2 
-		self.output = None
-		self.loadObjects()
-		self.loadFunctions(image1,image2)
-		self.loadLayout()
-		self.checkEquiv()
-	def loadObjects(self):
-		self.pix1 = QLabel(self)
-		self.pix2 = QLabel(self)
-		self.img1detail = QLabel(self)
-		self.img2detail = QLabel(self)
-		self.pick1 = QPushButton(self)
-		self.pick2 = QPushButton(self)
-		self.pick1.setMinimumHeight(50)
-		self.pick2.setMinimumHeight(50)
-	def loadFunctions(self, image1, image2):
-		self.img1detail.setText('\n'.join([(str(item)+':\t'+str(image1.__dict__[item])) for item in image1.__dict__ if item != 'transform']))
-		self.img2detail.setText('\n'.join([(str(item)+':\t'+str(image2.__dict__[item])) for item in image2.__dict__ if item != 'transform']))
-		# Load images
-		pixmap1 = QPixmap(image1._path+image1.src)
-		pixmap2 = QPixmap(image2._path+image2.src)
-		self.pix1.setPixmap( pixmap1.scaled(500, 500, Qt.KeepAspectRatio) )
-		self.pix2.setPixmap( pixmap2.scaled(500, 500, Qt.KeepAspectRatio) )
-		if pixmap1.isNull():
-			self.pix1.setText('Image not available.\nLikely due to incorrect path.')
-		if pixmap2.isNull():
-			self.pix2.setText('Image not available.\nLikely due to incorrect path.')
-		# Choose image buttons
-		self.pick1.setText('Choose this image')
-		self.pick2.setText('Choose this image')
-		self.pick1.clicked.connect( self.chooseImg )
-		self.pick2.clicked.connect( self.chooseImg )
-		# Adjust font/alignment
-		font = QFont('Arial',pointSize=18)
-		self.img1detail.setFont(font)
-		self.img2detail.setFont(font)
-		self.img1detail.setAlignment(Qt.AlignHCenter)
-		self.img2detail.setAlignment(Qt.AlignHCenter)
-		self.pix1.setFont(font)
-		self.pix2.setFont(font)
-		self.pix1.setAlignment(Qt.AlignHCenter)
-		self.pix2.setAlignment(Qt.AlignHCenter)
-	def loadLayout(self):
-		self.setWindowTitle('PyRECONSTRUCT Section Image Resolver')
-		hbox = QHBoxLayout()
-		# Left image
-		vbox1 = QVBoxLayout()
-		# vbox1.addWidget(self.img1label)
-		vbox1.addWidget(self.pix1)
-		vbox1.addWidget(self.img1detail)
-		vbox1.addWidget(self.pick1)
-		# Right image
-		vbox2 = QVBoxLayout()
-		# vbox2.addWidget(self.img2label)
-		vbox2.addWidget(self.pix2)
-		vbox2.addWidget(self.img2detail)
-		vbox2.addWidget(self.pick2)
-		hbox.addLayout(vbox1)
-		# hbox.addSpacing(50)
-		hbox.addLayout(vbox2)
-		self.setLayout(hbox)
-	def checkEquiv(self):
-		'''Check if images are equal. If so, automerge'''
-		if self.img1 == self.img2:
-			self.output = self.img1
-			self.pick1.setStyleSheet('background-color:lightgreen;')
-			self.pick2.setStyleSheet('background-color:lightgreen;')
-			self.pick1.setText('NO CONFLICT!')
-			self.pick2.setText('NO CONFLICT!')
-	def chooseImg(self):
-		if self.sender() == self.pick1:
-			self.output = self.img1
-			self.pick1.setStyleSheet('background-color:lightgreen;')
-			self.pick2.setStyleSheet(QWidget().styleSheet())
-		elif self.sender() == self.pick2:
-			self.output = self.img2
-			self.pick2.setStyleSheet('background-color:lightgreen;')
-			self.pick1.setStyleSheet(QWidget().styleSheet())
-		self.parent.isResolved()
 # - Contours
-class sectionContours(QWidget):
-	def __init__(self, uniqueA, compOvlp, confOvlp, uniqueB, sections=None, parent=None):
-		QWidget.__init__(self, parent)
-		self.parent = parent
-		self.setWindowTitle('PyRECONSTRUCT Section Contours Resolver')
-		self.conts1 = uniqueA+[ovlp[0] for ovlp in compOvlp]+[ovlp[0] for ovlp in confOvlp]
-		self.conts2 = uniqueB+[ovlp[1] for ovlp in compOvlp]+[ovlp[1] for ovlp in confOvlp]
+class SectionContourHandler(QWidget):
+	def __init__(self, MergeSection):
+	# (self, uniqueA, compOvlp, confOvlp, uniqueB, sections=None, parent=None):
+		QWidget.__init__(self)
+		self.merge = MergeSection
+		self.conts1 = self.merge.section1.contours
+		self.conts2 = self.merge.section2.contours
 		# input
-		self.uniqueA = uniqueA
-		self.uniqueB = uniqueB
-		self.compOvlp = compOvlp
-		self.confOvlp = confOvlp
-		if sections != None:
-			self.sections = sections
-			self.s1name = sections[0].name
-			self.s2name = sections[1].name
-		# output
-		self.output = None
+		self.uniqueA, self.uniqueB = self.merge.section1.getUniqueContours( self.merge.section2 )
+		self.compOvlp, self.confOvlp = self.merge.section1.getOverlappingContours( self.merge.section2, separate=True )
 		# Load UI
 		self.loadObjects()
 		self.loadFunctions()
 		self.loadLayout()
-		self.checkEquiv()
-	def checkEquiv(self):
-		'''Check if there are any unique contours. If not, call finish()'''
-		if (self.inUniqueA.count() == 0 and
-			self.inUniqueB.count() == 0 and
-			self.inOvlp.count() == 0):
-			# No conflicts
-			self.finish(parent=False)
-			self.doneBut.setText('NO CONFLICTS!')
 	def loadObjects(self):
 		# List contours in their appropriate listWidgets
 		self.inUniqueA = QListWidget(self)
@@ -320,7 +182,7 @@ class sectionContours(QWidget):
 		for table in [self.inUniqueA, self.inUniqueB, self.inOvlp, self.outUniqueA, self.outUniqueB, self.outOvlp]:
 			table.setSelectionMode(QAbstractItemView.ExtendedSelection)
 			table.itemDoubleClicked.connect(self.doubleClickCheck)
-		self.doneBut.setText('Merge')
+		self.doneBut.setText('Save')
 		self.doneBut.clicked.connect( self.finish )
 		self.doneBut.setMinimumHeight(50)
 		self.moveSelectedA.setText('Move Selected')
@@ -333,7 +195,7 @@ class sectionContours(QWidget):
 		container = QVBoxLayout()
 		secNameContainer = QHBoxLayout()
 		secNameContainer.setAlignment(Qt.AlignHCenter)
-		secNameContainer.addWidget(QLabel(str(self.s1name)+' vs. '+str(self.s2name)))
+		secNameContainer.addWidget(QLabel(str(self.merge.section1.name)+' vs. '+str(self.merge.section2.name)))
 		columnContainer = QHBoxLayout()
 		
 		labelContainer = QVBoxLayout()
@@ -374,7 +236,7 @@ class sectionContours(QWidget):
 		for item in items:
 			if item.__class__.__name__ == 'Contour' or type(item) == type([]):
 				# Item can be a contour or list of 2 contours, they are handled differently in contourTableItem class upon initialization
-				listItem = contourTableItem(item, [self.sections[0].image,self.sections[1].image])
+				listItem = contourTableItem(item, [self.merge.section1.image,self.merge.section2.image])
 				if type(item) == type([]):
 					if item in self.confOvlp: # Conflicting ovlping contour
 						listItem.setBackground(QColor('red'))
@@ -390,7 +252,7 @@ class sectionContours(QWidget):
 			item.clicked() # See contourTableItem class
 		self.doneBut.setStyleSheet(QWidget().styleSheet())
 	def moveItems(self):
-		# Move items in what table(s)?
+		# Move items in which table(s)?
 		if self.sender() == self.moveSelectedA:
 			inTable = self.inUniqueA
 			outTable = self.outUniqueA
@@ -409,10 +271,9 @@ class sectionContours(QWidget):
 			inTable.addItem( outTable.takeItem(outTable.row(item)) )
 		inTable.clearSelection()
 		outTable.clearSelection()
-		self.doneBut.setStyleSheet(QWidget().styleSheet())
-		self.output = None
-		self.parent.parent.clicked()
-	def finish(self, parent=True):
+		self.doneBut.setStyleSheet(QWidget().styleSheet()) # Button not green, indicates lack of save
+		self.merge.contours = None # Reset MergeSection.contours
+	def finish(self):
 		# Check ovlp table for unresolved conflicts (red)
 		numItems = self.outOvlp.count()
 		for i in range(numItems):
@@ -441,15 +302,15 @@ class sectionContours(QWidget):
 			msg.exec_()
 			return
 		# set self.output to chosen contours
-		self.output = [item.contour for item in oA]+[item.contour for item in oB]
+		output = [item.contour for item in oA]+[item.contour for item in oB]
 		for item in oO:
 			if type(item.contour) == type([]): # if both contours chosen in resolution
-				self.output.extend(item.contour)
+				output.extend(item.contour)
 			else:
-				self.output.append(item.contour)
-		self.doneBut.setStyleSheet('background-color:lightgreen;')
-		if parent:
-			self.parent.isResolved()
+				output.append(item.contour)
+		self.merge.contours = output
+		self.doneBut.setStyleSheet('background-color:lightgreen;') # Button to green
+
 class contourPixmap(QLabel):
 	'''QLabel that contains a contour drawn on its region in an image'''
 	def __init__(self, image, contour, pen=Qt.red):
