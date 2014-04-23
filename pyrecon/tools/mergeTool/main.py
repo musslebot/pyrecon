bb48a06cd82e59b13c22053b0338fdbed424a984
wtrdrnkr <mdmusslewhite@gmail.com>
2014-04-23 12:05:46 -0500

    MergeSet now properly manages arguments

--- pyrecon/tools/mergeTool/main.py
+++ pyrecon/tools/mergeTool/main.py
@@ -1,40 +1,20 @@
 from pyrecon.tools import handleXML as xml
 
-class MergeSet: #===
-	'''This object contains MergeSeries and MergeSection objects. This can then be passed into mergeTool functions.'''
+class MergeSet:
+	'''This class takes in a MergeSeries object and a list(MergeSection objects).'''
 	def __init__(self, *args, **kwargs):
 		self.seriesMerge = None # MergeSeries object
-		self.sectionMerge = None # List of MergeSection objects
-		self.output_directory = None
+		self.sectionMerges = None # List of MergeSection objects
 		self.processArguments(args, kwargs)
 	# Argument processing
 	def processArguments(self, args, kwargs):
-		self.processArgs(*args)
-		self.processKwargs(**kwargs)
-		self.checkAttributes()
-	def processArgs(self, *args): #===
-		return
-	def processKwargs(self, **kwargs): #===
-		return
-	def checkAttributes(self): #===
-		return
-	# Class methods
-	def run(self):
-		'''Performs merge for <series> and <sections>'''
-		self.seriesMerge.run()
-		for sectionMerge in self.sectionMerges:
-			sectionMerge.run()
-	def save(self):
-		if self.output_directory is None:
-			# Ask for output_directory #===
-			return
-		# Create mergedSeries from MergeSeries object
-		mergedSeries = self.series.output()
-		# For each MergeSection, create section and add to mergedSeries' sections
-		for section in self.sections:
-			mergedSeries.sections.append( section.output() )
-		# Write series/sections
-		xml.writeSeries(mergedSeries, self.output_directory, sections=True)
+		for arg in args:
+			if arg.__class__.__name__ == 'MergeSeries':
+				self.seriesMerge = arg
+			elif type(arg) == type([]):
+				self.sectionMerges = arg
+			else:
+				print 'Cannot process argument:',arg
 
 class MergeSection:
 	'''This class manages data about two Section objects that are undergoing a merge.'''
