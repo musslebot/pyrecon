b3b7efb11b4d6f0cc00e9126dda69a643a1eab27
wtrdrnkr <mdmusslewhite@gmail.com>
2014-08-10 18:58:06 -0500

    Fixed install_pyside.sh

--- install_pyside.sh
+++ install_pyside.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
-apt-get install build-essential git libqt4-dev libphonon-dev libxslt1-dev qtmobility-dev &&
-wget https://bootstrap.pypa.io/get-pip.py &&
-python get-pip.py &&
-pip install wheel &&
+sudo apt-get install build-essential git libqt4-dev libphonon-dev libxslt1-dev qtmobility-dev &&
 wget https://pypi.python.org/packages/source/P/PySide/PySide-1.2.2.tar.gz &&
 tar -xvzf PySide-1.2.2.tar.gz &&
 cd PySide-1.2.2 &&
-python setup.py bdist_wheel --qmake=/usr/bin/qmake-qt4 &&
-cd .. &&
+python setup.py install
+cd ..
+
