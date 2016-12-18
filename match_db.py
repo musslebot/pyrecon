334444e74369841e6bb3392fda70e9b81b3bafe0
Musslebot <musslebot@gmail.com>
2016-12-18 14:10:53 -0600

    dburi change

--- match_db.py
+++ match_db.py
@@ -6,7 +6,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 
-engine = create_engine("sqlite:///:memory:", echo=True)
+engine = create_engine("sqlite://", echo=True)
 
 Base = declarative_base()
 
