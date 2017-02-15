db24cd0a394886463a241a064a7fc78565e33ecc
Musslebot <musslebot@gmail.com>
2017-02-14 21:53:01 -0600

    2 contours

--- match_db.py
+++ match_db.py
@@ -19,6 +19,7 @@ class Contour(Base):
     """
     __tablename__ = "contours"
     id = Column(Integer, primary_key=True)
+    series = Column(Integer, nullable=False)
     section = Column(Integer, nullable=False)
     index = Column(Integer, nullable=False)
 
@@ -67,12 +68,21 @@ from pyrecon.tools.mergetool import is_contacting, is_exact_duplicate, is_potent
 
 # Read Reconstruct file into PyReconstruct
 # We're just playing around with a single Section here
-section = process_section_file("/home/musslebot/Downloads/CLZBJ 6.13.16/CLZBJ_final_elastic_done_v2 export.20")
+section_1 = process_section_file("/home/musslebot/Downloads/FHLTD.212")
+section_2 = process_section_file("/home/musslebot/Downloads/FHLTD.211")
 section_number = 20
 # Go through each Contour in the Section and create a DB Contour tuple (row)
-for i, cont in enumerate(section.contours):
+for i, cont in enumerate(section_1.contours):
     session.add(Contour(
         section=section_number,
+        series=1,
+        index=i
+    ))
+    session.commit()
+for i, cont in enumerate(section_2.contours):
+    session.add(Contour(
+        section=section_number,
+        series=2,
         index=i
     ))
     session.commit()
@@ -80,12 +90,12 @@ for i, cont in enumerate(section.contours):
 
 # Know we're going to categorize the PyReconstruct Contours by overlap
 # and then store a tuple (row) in the DB relation (table).
-def create_matches_from_contours(db_contours, section_contours):
+def create_matches_from_contours(db_contours_1, db_contours_2, section_contours_a, section_contours_b):
     matches = []
-    for idx, db_contour_A in enumerate(db_contours):
-        contA = section_contours[db_contour_A.index]
-        for idy, db_contour_B in enumerate(db_contours):
-            contB = section_contours[db_contour_B.index]
+    for idx, db_contour_A in enumerate(db_contours_1):
+        contA = section_contours_a[db_contour_A.index]
+        for idy, db_contour_B in enumerate(db_contours_2):
+            contB = section_contours_b[db_contour_B.index]
             if idx >= idy:  # TODO: verify logic here -- Does this make sense?
                 continue
             elif contA.name != contB.name:
@@ -112,12 +122,13 @@ def create_matches_from_contours(db_contours, section_contours):
     return matches
 
 
-def get_section_contours(section_number):
+def get_section_contours(series_number, section_number):
     """ Return all of the DB Contours that belong in the given section number.
     """
     return session.query(
         Contour
     ).filter(
+        Contour.series == series_number,
         Contour.section == section_number
     ).all()
 
@@ -132,11 +143,93 @@ def get_matches():
     ).all()
 
 
-db_contours = get_section_contours(section_number)
-section_contours = section.contours
-matches = create_matches_from_contours(db_contours, section_contours)
-# I set a breakpoint here and start messing around with queries and stuff.
-# Eventually I'll write a query that categorizes our ContourMatches by match_type
-# and pass that result into a function that combines that with information about
-# its corresponding Reconstruct Contour.
-import debug
+db_contours_1 = get_section_contours(1, section_number)
+db_contours_2 = get_section_contours(2, section_number)
+section_contours_a = section_1.contours
+section_contours_b = section_2.contours
+matches = create_matches_from_contours(
+    db_contours_1, db_contours_2,
+    section_contours_a, section_contours_b)
+# Commit matches to the DB
+session.add_all(matches)
+session.commit()
+
+from collections import defaultdict
+# Here we create an intermediate representation of our DB matches, structured
+# in such a way that it will allow us to easily fill in Reconstruct data for our UI
+# The data will be structured as follow:
+#     {
+#         <id of contour in db>: {
+#             "exact": <set of matching db contours>
+#             "potential": <set of matching db contours>
+#         }, ...
+#     }
+grouped = defaultdict(lambda: defaultdict(set))
+
+db_contour_ids = []
+results = session.query(Contour.id).all()
+# Results is a list of tuples: [(1), (2), (3), ...]
+for res in results:
+    db_contour_ids.append(res[0])
+
+for id_ in db_contour_ids:
+    matches = session.query(ContourMatch).filter(ContourMatch.id1 == id_).all()
+    grouped[id_] = defaultdict(set)
+    for m in matches:
+        grouped[id_][m.match_type].add(m.id2)
+
+# Fill intermediate representation with Reconstruct data relevant for the UI
+# [
+#     {
+#         'section': 1,
+#         'exacts': [[{}, {}], [{}, {}]...],
+#         'potentials': [[{}, {}, {}], [{}, {}]...],
+#         'uniques': [{}, {}, {}]
+#     },
+#     ...
+# ]
+
+section_matches = {
+    'section': 20,
+    'exact': [],
+    'potential': [],
+    'unique': []
+}
+
+already_added = []
+for contour_A_id, match_dict in grouped.iteritems():
+    db_contour_A = session.query(Contour).get(contour_A_id)
+    if db_contour_A.series == 1:
+        reconstruct_contour_a = section_1.contours[db_contour_A.index]
+        image = section_1.images[0]._path + "/{}".format(section_1.images[0].src),
+    elif db_contour_A.series == 2:
+        reconstruct_contour_a = section_2.contours[db_contour_A.index]
+        image = section_2.images[0]._path + "/{}".format(section_2.images[0].src),
+    main_contour_data = {
+        'name': reconstruct_contour_a.name,
+        'points': reconstruct_contour_a.points,
+        'image': image,
+        'db_id': contour_A_id
+    }
+    for match_type, matches in match_dict.iteritems():
+        match_list = [main_contour_data]
+        for match_id in matches:
+            already_added.append(match_id)
+            if db_contour_B.series == 1:
+                reconstruct_contour_b = section_1.contours[db_contour_B.index]
+                image = section_1.images[0]._path + "/{}".format(section_1.images[0].src),
+            elif db_contour_B.series == 2:
+                reconstruct_contour_b = section_2.contours[db_contour_B.index]
+                image = section_2.images[0]._path + "/{}".format(section_2.images[0].src),
+            db_contour_B = session.query(Contour).get(match_id)
+            match_list.append({
+                'name': reconstruct_contour_b.name,
+                'points': reconstruct_contour_b.points,
+                'image': image,
+                'db_id': match_id
+            })
+        section_matches[match_type].append(match_list)
+    if not match_dict.values():
+        section_matches["unique"].append(main_contour_data)
+    already_added.append(contour_A_id)
+
