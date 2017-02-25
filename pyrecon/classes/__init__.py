f8a0e73166703e9bdf911a4fbccfbb0d7ec03ab3
Musslebot <musslebot@gmail.com>
2017-02-25 15:05:07 -0600

    fixing import

--- pyrecon/classes/__init__.py
+++ pyrecon/classes/__init__.py
@@ -1,9 +1,9 @@
-from contour import Contour
-from image import Image
-from section import Section
-from series import Series
-from transform import Transform
-from zcontour import ZContour
+from .contour import Contour
+from .image import Image
+from .section import Section
+from .series import Series
+from .transform import Transform
+from .zcontour import ZContour
 
 
 __all__ = [
