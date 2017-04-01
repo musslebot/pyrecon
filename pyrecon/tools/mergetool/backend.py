54e5d3ea591495ef201b27cafe3111015586d9ea
Musslebot <musslebot@gmail.com>
2017-03-31 19:55:25 -0500

    WIP: debugger set at error pt

--- pyrecon/tools/mergetool/backend.py
+++ pyrecon/tools/mergetool/backend.py
@@ -78,30 +78,34 @@ def _create_db_contourmatch_from_db_contours_and_pyrecon_section(db_contour_A, d
 
     shape_a = pyrecon_contour_a.shape
     shape_b = pyrecon_contour_b.shape
-    if (pyrecon_contour_a.points == pyrecon_contour_b.points) and \
-       (pyrecon_contour_a.transform != pyrecon_contour_b.transform):
-        match_type = "potential_realigned"
-        return ContourMatch(
-            id1=db_contour_A.id,
-            id2=db_contour_B.id,
-            match_type=match_type
-        )
-    elif not is_contacting(shape_a, shape_b):
-        return None
-    elif is_exact_duplicate(shape_a, shape_b):
-        match_type = "exact"
-        return ContourMatch(
-            id1=db_contour_A.id,
-            id2=db_contour_B.id,
-            match_type=match_type
-        )
-    elif is_potential_duplicate(shape_a, shape_b):
-        match_type = "potential"
-        return ContourMatch(
-            id1=db_contour_A.id,
-            id2=db_contour_B.id,
-            match_type=match_type
-        )
+    try:
+        if (pyrecon_contour_a.points == pyrecon_contour_b.points) and \
+           (pyrecon_contour_a.transform != pyrecon_contour_b.transform):
+            match_type = "potential_realigned"
+            return ContourMatch(
+                id1=db_contour_A.id,
+                id2=db_contour_B.id,
+                match_type=match_type
+            )
+        elif not is_contacting(shape_a, shape_b):
+            return None
+        elif is_exact_duplicate(shape_a, shape_b):
+            match_type = "exact"
+            return ContourMatch(
+                id1=db_contour_A.id,
+                id2=db_contour_B.id,
+                match_type=match_type
+            )
+        elif is_potential_duplicate(shape_a, shape_b):
+            match_type = "potential"
+            return ContourMatch(
+                id1=db_contour_A.id,
+                id2=db_contour_B.id,
+                match_type=match_type
+            )
+    except Exception as e:
+        import pdb; pdb.set_trace()
+        print("{}".format(e))
     return None
 
 
